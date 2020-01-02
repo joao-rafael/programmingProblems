68ba7ca1fbadcdf2c30662e97fedd1c2557a0db5
João Rafael Silva <joaorafa131@gmail.com>
2020-01-02 17:28:30 -0300

    Update reverseString.js

--- JavaScript/reverseString.js
+++ JavaScript/reverseString.js
@@ -3,8 +3,8 @@ const reader = require('readline-sync');
 console.log('Reverse a string');
 const str = reader.question('Insert the string: \n');
 
+// code solution
 function reverseString(str) {
-  console.log(str);
   const reversed = [];
   for(let i = str.length-1 ; i >= 0; i--) {
     reversed.push(str[i]);
@@ -15,4 +15,12 @@ function reverseString(str) {
   return reversed;
 }
 
-console.log(reverseString(str));
+// js has a builtin method to it xD
+function reverse2(str){
+  return str.split('').reverse().join('');
+}
+
+// ES6 version
+const reverseES6 = str => str.split('').reverse().join('');
+
+console.log(reverse2(str));
