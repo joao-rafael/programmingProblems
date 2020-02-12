ec2f6bc6ba2d22e5c88ed9f6cf4b17ec683ea581
João Rafael Silva <joaorafa131@gmail.com>
2020-02-12 16:41:55 -0300

    Create HashTableImplementation.js
    this code implements a hashtable data structure in JavaScript, supported methods:
    set (insert)
    get

---
+++ JavaScript/HashTableImplementation.js
@@ -0,0 +1,41 @@
+class HashTable {
+  constructor(size){
+    this.data = Array(size);
+  }
+
+  _hashFn(hash, key, i){
+    return (hash + key.charCodeAt(i) * i) % this.data.length;
+  } 
+
+  _hash(key) {
+    let hash = 0;
+    for(let i = 0; i < key.length; i++){
+      hash = this._hashFn(hash, key, i);
+    }
+    return hash;
+  }
+  
+  _set(key, value){
+    let hashedKey = this._hash(key);
+    if(this.data[hashedKey] == undefined){
+      this.data[hashedKey] = [value];
+    }else{
+      console.log('Collision');
+      this.data[hashedKey].push(value);
+    }
+  }
+
+  _get(key){
+    let hashedKey = this._hash(key);
+    let found = this.data[hashedKey];
+    if(this.data[hashedKey] == undefined){
+      return 'not found';
+    }
+    return this.data[hashedKey];
+  }
+}
+
+/** code for testing/using */
+const myHashTable = new HashTable(50);
+myHashTable._set('grapes', 20);
+myHashTable._get('grapes');
